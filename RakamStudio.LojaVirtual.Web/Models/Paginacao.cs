32b525f32135c985afd64c6eb1ebc979e0622739
Gutavex <gutavex@gmail.com>
2016-03-22 16:43:46 -0300

    Classe de Paginação

---
+++ RakamStudio.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RakamStudio.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        // Itens totais no Banco de Dados
+        public int ItensTotal { get; set; }
+
+        // Itens que eu quero na página
+        public int ItensPorPagina { get; set; }
+
+        // Definiar página atual para manipular na tela (Pagina exibida no momento)
+        public int PaginaAtual { get; set; }
+
+        // Total de paginas na Paginação que teremos
+        public int TotalPaginas
+        {
+            get { return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina); }
+        }
+    }
+}
