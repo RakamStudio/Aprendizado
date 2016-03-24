1e598fd6eb258f4197d390a4c28fe94c8f23ca8b
Gutavex <gutavex@gmail.com>
2016-03-24 14:20:35 -0300

    Aula 9
    - Testes de Paginação
    - Configuração da HtmlHelpers

--- RakamStudio.LojaVirtual.UnitTest/UnitTest1.cs
+++ RakamStudio.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using RakamStudio.LojaVirtual.Web.Models;
+using RakamStudio.LojaVirtual.Web.HtmlHelpers;
 
 namespace RakamStudio.LojaVirtual.UnitTest
 {
@@ -7,8 +10,31 @@ namespace RakamStudio.LojaVirtual.UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestarPaginacao()
         {
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 10,
+                ItensTotal = 28
+            };
+
+
+            
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            // Act
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            // Assert
+
+            Equals(
+                @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>"
+                );
         }
     }
 }
